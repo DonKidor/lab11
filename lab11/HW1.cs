a8cff9390c24e15e2bd0513d5638eeb65e851dd0
Mihail <JIaIILLIa3@yandex.ru>
2022-05-28 10:19:50 +0700

    Add project

---
+++ lab11/HW1.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HW1
+{
+    public static long QueueTime(int[] customers, int n)
+    {
+        long[] q = new long[n];
+        for (int i = 0; i < n; i++) q[i] = 0;
+        foreach(int c in customers)
+        {
+            int minIndex = 0;
+            for(int j=0; j<n; j++)
+            {
+                if(q[j] < q[minIndex]) minIndex = j;
+            }
+            q[minIndex] += c;
+        }
+        long maxValue = q[0];
+        foreach(long val in q)
+        {
+            if (val > maxValue) maxValue = val;
+        }
+        return maxValue;
+    }
+}
---
+++ lab11/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class Programm
+{
+    public static void Main()
+    {
+        long res = HW1.QueueTime(new int[] {5,3,4}, 1);
+        Console.WriteLine(res);
+        res = HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+        Console.WriteLine(res);
+        res = HW1.QueueTime(new int[] {2,3,10},2);
+        Console.WriteLine(res);
+    }
+}
